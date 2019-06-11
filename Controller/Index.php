592df385f6cd75748887d818d301ab4e1f47df01
jasonweicn <jasonwei06@hotmail.com>
2019-06-11 23:20:04 +0800

    初始化

---
+++ Controller/Index.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Controller;
+
+use Mini\Base\Action;
+
+/**
+ * 这是一个控制器的案例
+ */
+class Index extends Action
+{
+    /**
+     * 初始化
+     */
+    function _init()
+    {
+        $this->view->title = 'MiniFramework Basic Application Template';
+        
+        // 使用布局，需在入口文件 Public/index.php 中定义常量 LAYOUT_ON 的值为 true
+        $this->view->_layout->setLayout('default');
+        $this->view->_layout->header = $this->view->render(LAYOUT_PATH . '/header.php');
+    }
+    
+    /**
+     * 默认动作
+     */
+    function indexAction()
+    {
+        // 实例化一个模型
+        $info = new \App\Model\Info();
+        
+        // 调用模型中的方法
+        $infoText = $info->getInfo();
+        
+        // 向View传值
+        $this->view->assign('info', $infoText);
+        
+        // 渲染并显示View
+        $this->view->display();
+    }
+}
