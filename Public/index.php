4106d007d0ef4f93fd167b6c28eb2b7920a1aa3d
jasonweicn <jasonwei06@hotmail.com>
2020-02-19 02:08:24 +0800

    配合框架新版发布对应用模板进行必要的更新

--- Public/index.php
+++ Public/index.php
@@ -3,24 +3,24 @@
  * 应用入口
  */
 
-// 应用命名空间
-define('APP_NAMESPACE', 'App');
+// 应用命名空间（请与应用所在目录名保持一致）
+const APP_NAMESPACE = 'App';
 
-// 应用路径
-define('APP_PATH',      dirname(dirname(__FILE__)));
+// 是否显示错误信息（默认值：false）
+const SHOW_ERROR = true;
 
-// 是否显示错误信息
-define('SHOW_ERROR',    true);
+// 是否开启日志（生产环境建议关闭，默认值：false）
+const LOG_ON = false;
 
-// 是否开启日志（生产环境建议关闭）
-define('LOG_ON',        false);
-
-// 是否启用布局功能
-define('LAYOUT_ON',     true);
+// 是否启用布局功能（默认值：false）
+const LAYOUT_ON = true;
 
 // 是否开启REST模式的API接口功能（默认值：false）
-define('REST_ON',       false);
+const REST_ON = true;
 
-// 引入 MiniFramework
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/jasonweicn/miniframework/Bootstrap.php';
+// 兼容多平台的目录分隔符
+const DS = DIRECTORY_SEPARATOR;
+
+// 引入 MiniFramework 就是这么简单
+require dirname(__DIR__) . DS . 'vendor' . DS . 'autoload.php';
+require dirname(__DIR__) . DS . 'vendor' . DS . 'jasonweicn' . DS . 'miniframework' . DS . 'Bootstrap.php';
